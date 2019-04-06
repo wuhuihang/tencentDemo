f9fe8795564301c5094e51d8fc1b6be5e7b42019
wuhuihang908 <125740601@qq.com>
2019-04-06 13:34:47 +0800

    add reaponse.vue

--- demo/src/view/h5/responsive.vue
+++ demo/src/view/h5/responsive.vue
@@ -1,6 +1,32 @@
 <template>
   <article class="responsive">
-    <div>responsive</div>
+    <div class="slide">750x340</div>
+    <div class="banner">
+      <img>
+    </div>
+    <nav class="flex">
+      <a>
+        <div>80x80</div>
+        <div class="name">导航1</div>
+      </a>
+      <a>
+        <div>80x80</div>
+        <div class="name">导航2</div>
+      </a>
+      <a>
+        <div>80x80</div>
+        <div class="name">导航3</div>
+      </a>
+      <a>
+        <div>80x80</div>
+        <div class="name">导航4</div>
+      </a>
+      <a>
+        <div>80x80</div>
+        <div class="name">导航5</div>
+      </a>
+    </nav>
+    <div class="sale">750x286</div>
   </article>
 </template>
 
@@ -13,5 +39,50 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+@import "~@@/mixin.less";
+.slide {
+  height: 170 / @r;
+  background: #ccc;
+  text-align: center;
+  .ft(25 / @r, 170 / @r, #666);
+}
+.banner {
+  .wh(100%, 0);
+  position: relative;
+  background: #eee;
+  padding-top: 13.33333%;
+  img {
+    .wh(100%, 100%);
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+}
+.flex {
+  display: flex;
+  padding: 10 / @r;
+  font-size: 12 / @r;
+  text-align: center;
+  a {
+    flex: 1;
+    display: block;
+    div:first-child {
+      .wh(40 / @r, 40 / @r);
+      background: #cbcbcb;
+      margin: 0 auto;
+      line-height: 40 / @r;
+    }
+    .name {
+      font-size: 12 / @r;
+      text-align: center;
+    }
+  }
+}
+.sale {
+  height: 143 / @r;
+  background: #eee;
+  text-align: center;
+  .ft(25 / @r, 143 / @r, #666);
+}
 </style>
